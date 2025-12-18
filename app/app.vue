000d6490308991e85a2d5c932b070fe75e4f5f35
Huy Vu <huy_htv@hotmail.com>
2025-12-18 13:32:15 +0100

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+
+<!-- 
+| Folder         | Nuxt does               |
+| -------------- | ----------------------- |
+| `pages/`       | Auto-creates routes     |
+| `components/`  | Auto-imports components |
+| `layouts/`     | Makes layouts available |
+| `assets/`      | Processes CSS & images  |
+-->
