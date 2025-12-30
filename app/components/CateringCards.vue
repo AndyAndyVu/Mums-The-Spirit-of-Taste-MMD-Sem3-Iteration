f3624b8ebde2101703fe7968c8338a2c77fca9ce
Emilia-v-p19 <emilia@pretsch.dk>
2025-12-30 12:12:04 +0100

    add catering intro og cards

---
+++ app/components/CateringCards.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+</script>
+
+<template>
+</section>
+<article class="caCard">
+    <img src="../assets/pictures/cateTapas.png" alt="billede af tapas menu, med røget hjortekød, røget laks, Porre/løgtærte, rejer på madpandekage og Pork Belly med chutney">
+    <h3>Tapas</h3>
+    <p>Vi ønsker, med vores nordiske tapas koncept, at giver dig mulighed for at sammensætte din helt egen menu af fantistiske små retter.</p>
+</article>
+<article class="caCard">
+    <img src="../assets/pictures/cateMad.png" alt="billede af Mad ud af huset menu, med kød, salt, kartoffler, sauce, Bowls og tapas">
+    <h3>Mad ud af huset</h3>
+    <p>Holder I festen hjemme og vil du gerne slippe for at knokle i køkkenet? Så lad MUMS levere en kulinarisk selskabs buffet, med alt hvad dertil hører af lækkerier.</p>
+</article>
+<article class="caCard">
+    <img src="../assets/pictures/cateRecep.png" alt="billede af Receptions-menu, med sliders, røget laks, Håndpillede rejer, Rørt oksetatar og Brownie">
+    <h3>Receptions-menu</h3>
+    <p>Vores fantastiske menuer er skræddersyet til enhver fest og garanterer at tilfredsstille enhver smag og appetit. Så hvorfor bruge tid på at bekymre dig om maden, når du kan stole på os til at levere en mindeværdig madoplevelse.</p>
+</article>
+<article class="caCard">
+    <img src="../assets/pictures/cateMode.png" alt="billede af møde tallerken, med porre/løgtærte ,Røget okseinderlår, Slider, Røget skinke og Dessertkage">
+    <h3>Møde tallerken</h3>
+    <p>En mødetallerken leveres anrettet på flot engangsservice lige til at servere, der medfølger friskbagt brød og Unika smør – du skal kun supplere med service og drikkevarer, så er frokosten klar.</p>
+</article>
+<article class="caCard">
+    <img src="../assets/pictures/cateJul.png" alt="billede af jule menu, med fiskefille, jule-sliders, frikadeller,kyllingesalat og Ris a la mande">
+    <h3>Jule-menu</h3>
+    <p>Vores jule- og nytårsmenu er igen i år skabt med respekt for gode, lokale råvarer– leveret af nogle af de bedste producenter i Nordjylland og resten af Danmark.</p>
+</article>
+<article class="caCard">
+    <img src="../assets/pictures/cateBrunch.png" alt="billede af brunch menu, med bacon, æg, lækkeret brød og musli">
+    <h3>Brunch</h3>
+    <p>Vores brunch er bygget op af gode, lokale råvarer, og vores fooddesignere sætter en ære i at kreere den bedst mulige mad inspireret af den smukke, danske natur, der omgiver vores dejlige Aalborg.</p>
+</article>
+<section>
+</template>
+
+<style scoped>
+
+</style>
--- app/pages/catering.vue
+++ app/pages/catering.vue
@@ -1,1 +1,23 @@
-<template>s</template>
+<script setup>
+
+</script>
+
+<template>
+<main>
+    <section>
+    <div class="cateringIntro">
+    <h1>Catering Menu</h1>
+    <p>Vores catering er lavet fra bunden med kærlighed til smag, håndværk og gode råvarer.Til både hverdag og fest og lige nu også lækker julecatering.</p>
+    <button><a href="https://takeaway.mumsshop.dk/">Bestil Catering her</a></button></div>
+    <img src="../assets/pictures/cateringIntro.png" alt="billede af en af mums lækkre catering menuer ">
+</section>
+<section>
+    <CateringCards/>
+</section>
+</main>
+<FooterDesk/>
+</template>
+
+<style scoped>
+
+</style>
