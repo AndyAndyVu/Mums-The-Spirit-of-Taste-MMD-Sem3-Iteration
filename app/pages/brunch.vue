f8481c207986a3f49e2ffa92670c8573ab46e804
Andy <andyandyvu@gmail.com>
2026-01-05 19:06:58 +0100

    add side af brunch

--- app/pages/brunch.vue
+++ app/pages/brunch.vue
@@ -1,16 +1,45 @@
 <template>
   <NavDesk />
   <main>
-    <section>
-      <div>
+    <section class="brunch-container">
+      <div class="text-brunch">
         <h1>Brunch</h1>
-        <p></p>
+        <p>
+          Hvem kan sige nej til god, nordisk gourmetmad taget med fra
+          barndomskøkkenet og mors kyndige hænder? Ikke os! Vores brunch er
+          bygget op af gode, lokale råvarer, og vores fooddesignere sætter en
+          ære i at kreere den bedst mulige mad inspireret af den smukke, danske
+          natur, der omgiver vores dejlige Aalborg.
+        </p>
+        <p>
+          Sæt tempoet ned, tag en dyb indånding og lad os sørge for dig: Hvad
+          end du er til hjemmelavet jordbærsyltetøj eller leverpostej,
+          anstrenger vi os for at leve op til dine standarder. Med brunch-buffet
+          skal du ikke vente på maden, men du behøver heller ikke skynde dig!
+        </p>
+        <p>
+          Vi er stolte over at kunne præsentere et morgenbord, der ikke bare er
+          nemt, flot og rustikt, men også støtter leverandører fra nærområdet og
+          holder gamle traditioner i hævd.
+        </p>
+        <p>Der kan tilkøbes varm leverpostej til brunchen.</p>
+        <p>Brunch skal bestilles senest dagen før afh. inden kl. 12:00</p>
       </div>
       <figure>
-        <img src="" alt="" />
+        <img src="../assets/img/brunch-MUMS.svg" alt="Lækker brunch fra MUMS" />
       </figure>
-      <Pattern />
     </section>
+    <Pattern />
   </main>
   <FooterDesk />
 </template>
+
+<style scoped>
+.brunch-container {
+  display: flex;
+}
+
+.text-brunch {
+  max-width: 75ch;
+}
+</style>
