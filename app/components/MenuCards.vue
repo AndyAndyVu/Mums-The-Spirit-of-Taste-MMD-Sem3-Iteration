532c98f8084171ddaef4a6d78564b98608a7556b
Huy Vu <huy_htv@hotmail.com>
2026-01-05 10:11:03 +0100

    add MenuCards.vue

---
+++ app/components/MenuCards.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+<div class="takeAwayCard">
+    <img 
+    :src="menuItem.image.src" 
+    :alt="menuItem.image.alt">
+    <div>
+        <h4>{{ menuItem.title }}</h4>
+        <p>{{ menuItem.description }}</p>
+        <h4>{{ menuItem.price }}</h4>
+    </div>
+    <div>
+        <p>{{ menuItem.allergies }}</p>
+    </div>
+</div>
+</template>
+
+<style scoped>
+</style>
