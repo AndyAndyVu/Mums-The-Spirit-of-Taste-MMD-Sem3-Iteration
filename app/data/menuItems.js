a529bbc7d43feafdfa9d8da2ef2be909a0b59dd9
Huy Vu <huy_htv@hotmail.com>
2026-01-05 10:34:09 +0100

    add dataset menuItems.js
    not all data inserted yet, atm only burger & tapas

---
+++ app/data/menuItems.js
@@ -0,0 +1,375 @@
+export const menuItems = [
+  // ===== BURGERE (1–99) =====
+  {
+    id: 1,
+    category: "burger",
+    title: "MUMS Burger",
+    description:
+      "En bøf af dansk oksekød, hvidløgs mayo, tomat relish, rødløg, tørsaltet bacon, cheddar ost og salat.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 2,
+    category: "burger",
+    title: "Vesterhav Burger",
+    description:
+      "Dansk oksebøf af 100% dansk oksekød, Thise Vesterhav ost, tynde slices af danske rødløg, hjemmelavet agurkesalat og hjemmerørt hvidløg- og chili mayo.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 3,
+    category: "burger",
+    title: "Jubilæums Burger",
+    description:
+      "Juicy burger med bøf af dansk oksekød, spicy chili cheese tops, hvidløgs- og chili mayo, hjemmesyltet rødløg, tomat relish, cheddar ost og salat.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 4,
+    category: "burger",
+    title: "Den Nordjyske Bøf Sandwich",
+    description:
+      "En bøf af dansk oksekød, remoulade, tomat relish, syltede agurker, rødbeder, 3 slags løg (rå, friteret og hjemmesyltet) – leveres med brun sovs.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 5,
+    category: "burger",
+    title: "Kyllinge Burger",
+    description:
+      "Grov paneret brystfilet lavet af 100% dansk velfærdskylling fra Happy Chicken, tørsaltet bacon, chili mayo, syltede agurker, tomat relish og MUMS hjemmelavet dressing.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 6,
+    category: "burger",
+    title: "Flæskestegs Burger",
+    description:
+      "Saftig grisekam, hjemmelavet rødkål, agurkesalat, hvidløgsmayo og sprøde svær.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 7,
+    category: "burger",
+    title: "Vegetar Burger",
+    description:
+      "Vegetar bøf, hvidløgs mayo, tomat relish, rødløg, cheddar ost og salat.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "soja", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 8,
+    category: "burger",
+    title: "Junior Burger",
+    description:
+      "En bøf af 100% dansk oksekød med hvidløgs mayo, ketchup, bacon, cheddar ost og salat.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "sennep", "selleri"],
+    image: null,
+  },
+
+  // ===== TAPAS (101–199) =====
+  {
+    id: 101,
+    category: "tapas",
+    title: "MUMS Mega Kartoffelchips",
+    description:
+      "MUMS hjemmelavet kartoffelchips med Mariager sydesalt.",
+    price: null,
+    allergies: [],
+    image: null,
+  },
+  {
+    id: 102,
+    category: "tapas",
+    title: "Sprøde Flæskesvær",
+    description:
+      "Hjemmelavede flæskesvær fra KrastrupSøgaard – sprøde, saltede og dejligt knasende. En snack i verdensklasse.",
+    price: null,
+    allergies: [],
+    image: null,
+  },
+  {
+    id: 103,
+    category: "tapas",
+    title: "Oliven Mix med Rosmarin",
+    description:
+      "Kombination af grønne og sorte oliven i olie. Friske, sunde og skønne – en hygge-snack der er god for hjertet.",
+    price: null,
+    allergies: [],
+    image: null,
+  },
+  {
+    id: 104,
+    category: "tapas",
+    title: "MUMS Egen Nøddeblanding",
+    description:
+      "Dejlig nøddeblanding med Valencia mandler, peanuts og cashewnødder.",
+    price: null,
+    allergies: ["nødder", "jordnødder"],
+    image: null,
+  },
+  {
+    id: 105,
+    category: "tapas",
+    title: "Røget Laks",
+    description:
+      "Grand cru laks fra Polar Sea Food i Vodskov. Koldrøget over bøgeflis, hjemmesyltet perleløg og dild mayo.",
+    price: null,
+    allergies: ["æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 106,
+    category: "tapas",
+    title: "Urtehaven",
+    description:
+      "Urtehaven tærteform med bund af hjemmerørt humus, ristede sesam, peanuts og grønne asparges.",
+    price: null,
+    allergies: ["gluten", "peanuts"],
+    image: null,
+  },
+  {
+    id: 107,
+    category: "tapas",
+    title: "Kyllingesalat",
+    description:
+      "Hjemmelavet kyllingesalat, grønne asparges og bacon.",
+    price: null,
+    allergies: ["æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 108,
+    category: "tapas",
+    title: "Rejer på Madpandekage",
+    description:
+      "Håndpillede rejer fra Grønland på madpandekage, hjemmelavet dild mayo, citron støv og frisk dild.",
+    price: null,
+    allergies: ["krebsdyr", "gluten", "mælk", "æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 109,
+    category: "tapas",
+    title: "Porre/Løgtærte",
+    description:
+      "Porre/løgtærte bagt med revet ost, pyntet med mayo og frisk salat.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 110,
+    category: "tapas",
+    title: "Pulled Beef",
+    description:
+      "Hjemmelavet pulled beef af Dansk Gastro Kalv med teriyaki sauce, salsamix og chili mayo, serveret på flatbrød.",
+    price: null,
+    allergies: ["gluten", "mælk", "soja", "æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 111,
+    category: "tapas",
+    title: "Danske Velfærdskylling",
+    description:
+      "Paneret brystfilet af dansk velfærdskylling med rosmarin og citron, street food dressing, purløg og ristet sesam, på nordisk flatbrød.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg", "soja", "sennep"],
+    image: null,
+  },
+  {
+    id: 112,
+    category: "tapas",
+    title: "Røget Okseinderlår",
+    description:
+      "Røget okseinderlår, langtidstørret fra KrastrupSøgaard med bruschetta mayo og oliven.",
+    price: null,
+    allergies: ["mælk", "æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 113,
+    category: "tapas",
+    title: "Røget Hjortekød",
+    description:
+      "Røget hjorteinderlår fra KrastrupSøgaard med peberrodscreme og ribs – en unik smagsoplevelse.",
+    price: null,
+    allergies: ["mælk"],
+    image: null,
+  },
+  {
+    id: 114,
+    category: "tapas",
+    title: "Røget Skinke",
+    description:
+      "Røget Grand Cru skinke fra KrastrupSøgaard med hjemmelavet æggestand, mayo, soltørret tomat og frisk purløg.",
+    price: null,
+    allergies: ["mælk", "æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 115,
+    category: "tapas",
+    title: "Roastbeef",
+    description:
+      "Klassisk roastbeef med grov remoulade, bløde løg og agurkesalat.",
+    price: null,
+    allergies: ["gluten", "æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 116,
+    category: "tapas",
+    title: "Kyllingefilet",
+    description:
+      "Kyllingeinderfilet med mayo og grillede peberfrugter – fin og raffineret i smagen.",
+    price: null,
+    allergies: ["æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 117,
+    category: "tapas",
+    title: "Filet Royal",
+    description:
+      "Filet Royal af Dansk tender pork med syltet rødløg og trøffel mayo.",
+    price: null,
+    allergies: ["æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 118,
+    category: "tapas",
+    title: "Vitello Tonnato",
+    description:
+      "Vitello Tonnato lavet på kalveribeye fra Dansk Gastro Kalv, med klassisk tuncreme og kapers.",
+    price: null,
+    allergies: ["æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 119,
+    category: "tapas",
+    title: "Slider Pulled Pork",
+    description:
+      "Slider med hjemmelavet pulled pork af Dansk Bertels gris med BBQ sauce, sennepsmayo og hjemmesyltet agurk.",
+    price: null,
+    allergies: ["gluten", "æg", "sennep"],
+    image: null,
+  },
+  {
+    id: 120,
+    category: "tapas",
+    title: "Slider Kyllingeinderfilet",
+    description:
+      "Slider med grillet kyllingeinderfilet, spicy mayo, syltet rød peber og street food dressing.",
+    price: null,
+    allergies: ["gluten", "æg", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 121,
+    category: "tapas",
+    title: "Slider med Avocado",
+    description:
+      "Slider med avocado, vegansk mayo, syltet peber, salsamix og smoky grill dressing.",
+    price: null,
+    allergies: ["gluten", "sennep"],
+    image: null,
+  },
+  {
+    id: 122,
+    category: "tapas",
+    title: "Nordisk Fladbrød",
+    description:
+      "Nordisk flatbrød med bagt peberfrugt, hjemmerørt humus og salsamix.",
+    price: null,
+    allergies: ["gluten"],
+    image: null,
+  },
+  {
+    id: 123,
+    category: "tapas",
+    title: "Vegansk Bøf med BBQ",
+    description:
+      "Vegansk bøf med BBQ dressing, vegansk mayo, purløg og kornblomster.",
+    price: null,
+    allergies: ["soja", "gluten", "sennep", "selleri"],
+    image: null,
+  },
+  {
+    id: 124,
+    category: "tapas",
+    title: "Pork Belly med Chutney",
+    description:
+      "Pork belly af Tender Pork med ananas-peber chutney og grønt.",
+    price: null,
+    allergies: ["æg", "selleri"],
+    image: null,
+  },
+  {
+    id: 125,
+    category: "tapas",
+    title: "Osteanretning",
+    description:
+      "Osteanretning med 3 forskellige danske oste og hjemmelavet sylt.",
+    price: null,
+    allergies: ["mælk"],
+    image: null,
+  },
+  {
+    id: 126,
+    category: "tapas",
+    title: "Dessertkage",
+    description:
+      "En overdådig chokoladekage med fløjlsblød mørk- og hvid chokolademousse.",
+    price: null,
+    allergies: ["mælk", "soja", "æg", "gluten"],
+    image: null,
+  },
+  {
+    id: 127,
+    category: "tapas",
+    title: "Gammeldags Æblekage",
+    description:
+      "MUMSs gammeldags æblekage med makroner og flødeskum.",
+    price: null,
+    allergies: ["gluten", "mælk", "æg"],
+    image: null,
+  },
+  {
+    id: 128,
+    category: "tapas",
+    title: "Passions/Mango Mousse Kage",
+    description:
+      "Vegansk passions- og mango mousse kage med crumble-bund.",
+    price: null,
+    allergies: ["gluten", "soja"],
+    image: null,
+  },
+  {
+    id: 129,
+    category: "tapas",
+    title: "Panna Cotta",
+    description:
+      "Cremet panna cotta med hjemmelavet skovbærsylt.",
+    price: null,
+    allergies: ["mælk"],
+    image: null,
+  },
+]
