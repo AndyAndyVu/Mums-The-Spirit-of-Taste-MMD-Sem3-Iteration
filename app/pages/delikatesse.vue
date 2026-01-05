968c44dcf54b28f8e90f1a3da9c5b622e78e0d51
Andy <andyandyvu@gmail.com>
2026-01-05 10:02:29 +0100

    add delikatesse side
    tilføjelse af siden samt styling + billeder

--- app/pages/delikatesse.vue
+++ app/pages/delikatesse.vue
@@ -1,1 +1,47 @@
-<template>s</template>
+<template>
+  <NavDesk />
+  <section class="deli-box">
+    <div class="deli-text">
+      <h1>Delikatesse</h1>
+      <p>
+        Glæd familie, venner, medarebjdere og forretningsforbindelser med en
+        gave der smager af MUMS.
+      </p>
+      <p>
+        Vi har udvalgte vine, oste, chokolade og andre lækkerier. Så kom ned i
+        butikken og gå på opdagelse, lad dig inspirere og få personlig
+        vejledning.
+      </p>
+      <p>
+        Hos os er gavepakker en oplevelse, sammensat med omhu, kvalitet og
+        kærlighed. Vi har samlet de bedste danske og udenlandske producenter og
+        er glade for også at kunne præsentere flere hjemmelavede produkter. Vi
+        har sammensat et udvalg af gaver som alle pakkes eksklusivt og flot ind.
+        Ønsker du selv at samemsætte din gave kan det også lade sig gøres.
+      </p>
+    </div>
+    <div>
+      <img src="../assets/img/Delikatesse.jpg" alt="salgAfVarer" />
+    </div>
+  </section>
+  <section class="slide-show">Mangler..</section>
+  <img src="../assets/img/" alt="" />
+  <section class="gave-kurv"></section>
+</template>
+
+<style scoped>
+.deli-box {
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem;
+}
+
+.deli-box img {
+  width: 100%;
+  height: auto;
+}
+
+.deli-text {
+  max-width: 85ch;
+}
+</style>
