752f843e9f0fe63731d90b237e5a5c85432de6ed
Huy Vu <10643808@ucn.dk>
2026-01-06 21:35:50 +0100

    Update menu.vue

--- app/pages/menu.vue
+++ app/pages/menu.vue
@@ -18,38 +18,79 @@
       </div>
       <img src="https://placehold.co/600x500?text=Food" alt="Hero billede af takeaway mad" />
     </section>
-    <aside>
-      <a href="">Burger</a>
-      <a href="">Tapas</a>
-      <a href="">Sandwhich</a>
-      <a href="">Brunch</a>
-      <a href="">Nem Aftensmad</a>
-      <a href="">Bowls</a>
-      <a href="">Boa Buns & Sides</a>
-      <a href="">Børne Menu</a>
-      <a href="">Vegansk</a>
-      <a href="">Møde Tallerken</a>
-      <a href="">Dessert</a>
-    </aside>
-    <div>
-      <div>
-        <button>Mad Type</button>
-        <button>Allegener</button>
-        <button>Diæt</button>
-      </div>
-      <button>Sortering</button>
+    <div class="menuContainer">
+      <aside>
+        <a href="">Burger</a>
+        <a href="">Tapas</a>
+        <a href="">Sandwhich</a>
+        <a href="">Brunch</a>
+        <a href="">Nem Aftensmad</a>
+        <a href="">Bowls</a>
+        <a href="">Bao Buns & Sides</a>
+        <a href="">Børne Menu</a>
+        <a href="">Vegansk</a>
+        <a href="">Møde Tallerken</a>
+        <a href="">Dessert</a>
+      </aside>
+      <section class="menuCardsSection">
+        <div class="controlButtons">
+          <div class="filterButtons">
+            <button>Mad Type</button>
+            <button>Allegener</button>
+            <button>Diæt</button>
+          </div>
+          <button class="sortingButton">Sortering</button>
+        </div>
+        <MenuCards />
+      </section>
     </div>
-    <MenuCards />
   </main>
   <FooterDesk />
 </template>
 
 <style scoped>
-  section {
-    display: flex;
-    justify-content: space-between;
-    gap: 1rem;
-    margin-top: 2rem;
-  }
- 
+section {
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  margin-top: 2rem;
+}
+
+.menuContainer {
+  display: flex;
+  gap: 1rem;
+  margin-top: 2rem;
+}
+
+aside {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  width: max-content;
+  height: fit-content;
+  
+  padding: 1rem 2rem;
+  background-color: var(--blaa);
+}
+aside a {
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+  white-space: nowrap;
+}
+.menuCardsSection {
+  display: flex;
+  flex-direction: column;
+  margin-top: 0;
+}
+.controlButtons {
+  display: flex;
+  justify-content: space-between;
+}
+.filterButtons {
+  display: flex;
+  gap: 1rem;
+  margin: 0 1rem;
+}
 </style>
