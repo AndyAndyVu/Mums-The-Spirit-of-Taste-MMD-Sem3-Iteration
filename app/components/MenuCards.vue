a67529f97c693ad9e896d1dd6c2ba74df8c6a842
Huy Vu <10643808@ucn.dk>
2026-01-07 12:10:13 +0100

    Update MenuCards.vue
    add category and description

--- app/components/MenuCards.vue
+++ app/components/MenuCards.vue
@@ -4,42 +4,51 @@ import { takeawayMenu } from '../data/menuItems.js';
 
 <template>
     <div class="menuSections">
-        <section v-for="category in takeawayMenu" :key="category.id" v-if="category.items.length"
-            class="categorySection" :id="category.id">
-            <header class="categoryHeader">
-                <h2>{{ category.title }}</h2>
-                <p>{{ category.description }}</p>
-            </header>
+        <section v-for="category in takeawayMenu" :key="category.id" class="categorySection" :id="category.id">
+            <template v-if="category.items && category.items.length">
+                <div class="categoryHeader">
+                    <h2>{{ category.title }}</h2>
+                    <p>{{ category.description }}</p>
+                </div>
 
-            <div class="takeAwayContainer">
-                <div class="takeAwayCard" v-for="item in category.items" :key="item.id">
-                    <img :src="item.image" :alt="item.title" />
-                    <div class="takeAwayCardInfo">
-                        <div>
-                            <h4>{{ item.title }}</h4>
-                            <p>{{ item.description }}</p>
-                            <h4>{{ item.price }}</h4>
-                        </div>
-                        <div>
-                            <p>{{ item.allergies.join(', ') }}</p>
+                <div class="takeAwayContainer">
+                    <div class="takeAwayCard" v-for="item in category.items" :key="item.id">
+                        <img :src="item.image" :alt="item.title" />
+
+                        <div class="takeAwayCardInfo">
+                            <div>
+                                <h4>{{ item.title }}</h4>
+                                <p>{{ item.description }}</p>
+                                <h4>{{ item.price }},-</h4>
+                            </div>
+
+                            <p v-if="item.allergies?.length">
+                                {{ item.allergies.join(', ') }}
+                            </p>
                         </div>
                     </div>
                 </div>
-            </div>
+            </template>
         </section>
     </div>
 </template>
+
 
 <style scoped>
 img {
     width: 100%;
     height: auto;
 }
-
+.categoryHeader{
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin: 2rem 0 1rem 0;
+}
 .takeAwayContainer {
     width: 100%;
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     gap: 1rem;
 }
 
@@ -64,7 +73,7 @@ img {
     gap: 0.5rem;
 }
 
-.takeAwayCardInfo>div:last-child {
+.takeAwayCardInfo>p {
     margin-top: auto;
 }
 </style>
