afc34b2a0e66aca0f6ef1602e9ea2a397c4cf4e8
Huy Vu <huy_htv@hotmail.com>
2026-01-08 15:34:20 +0100

    Create test.vue

---
+++ app/pages/test.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {allTakeAwayItems} from "../composable/takeAwayFlatMap.js";
+
+// kører med det samme når siden loader
+console.log("All items:", allTakeAwayItems);
+console.table(allTakeAwayItems);
+</script>
+
+<template>
+    <div>
+        <h1>Test output – se console</h1>
+    </div>
+</template>
+
+<style scoped></style>
