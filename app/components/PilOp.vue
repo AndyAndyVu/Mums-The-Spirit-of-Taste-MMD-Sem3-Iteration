defdc68699c4f81d15be58df41598096ce3cd3fe
Emilia-v-p19 <emilia@pretsch.dk>
2026-01-14 09:31:51 +0100

    pil virker nu

--- app/components/PilOp.vue
+++ app/components/PilOp.vue
@@ -1,10 +1,35 @@
 <script setup>
+import { onMounted } from "vue"
+
+onMounted(() => {
+  const pilOp = document.querySelector(".pilOp")
+
+//Naar brugen scrolle skal knappen komme fre med funktion visKnap
+window.onscroll = function() {visKnap()};
+//holder øje med hvor langt brugen er scrollede vis de er scrolled mere end 50px
+// skal knapen vise sig
+function visKnap(){
+if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550){
+    pilOp.style.display = "block";} //viser knappen
+    else
+    {pilOp.style.display = "none"; }//vis ikke der bliver scrollede 400px skal knappen ikke vises   
+}
+
+//topknapen får nu en eventlistner hvor vis den bliver trygede går den til toppen. hjælp fundet https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo
+pilOp.addEventListener("click", function() {//laver en function til topknap når den bliver cliked
+  window.scrollTo({ //dette scroller paa siden
+    top: 0, //her scroller den helt til toppen
+    behavior: "smooth"
+  });
+})
+});
+
 
 </script>
 
 <template>
 <div>
-<button class="pilOp visKnap"><img src="../assets/pictures/MumsPilOp.svg"></button>
+<button class="pilOp"><img src="../assets/pictures/MumsPilOp.svg"></button>
 </div>
 </template>
 
@@ -13,7 +38,7 @@
 .pilOp{
     background-color: rgba(255, 255, 255, 0);
     border: none;
-    display: block;
+    display: none;
     position: fixed;
     bottom: 0.5rem;
     right: 1rem;
